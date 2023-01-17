ceeed20d3089ddfeb40e25102118c8896dc6a7af
Zaur-cmd <76215568+Zaur-cmd@users.noreply.github.com>
2023-01-17 22:38:56 +0500

    Updated search functionality

--- application/controllers/Search.php
+++ application/controllers/Search.php
@@ -23,15 +23,17 @@ class Search extends REST_Controller
      */
     public function with_price_get()
     {
-        $data['srch_inp'] = $this->input->get("srch_pr_inp");
+       $srch_pr_inp = $this->replaceStr($this->input->get("srch_pr_inp"));
+        $data['srch_inp'] = $srch_pr_inp;
+
         $res = $this->product->search_for_prod(
-            $this->input->get("srch_pr_inp"),
+            $srch_pr_inp,
             $this->input->get("min_price"),
             $this->input->get("max_price"),
             $this->input->get("user_id")
 
         );
-//        var_dump($res);
+
         if ($res) {
             $newArray  = $res;
             if (sizeof($newArray) != 0) {
@@ -49,7 +51,7 @@ class Search extends REST_Controller
 
         $result = [
             'data' => $data,
-            'products' => $newArray ?? []
+            'productsd' => $newArray ?? []
         ];
 
         $this->response($result, REST_Controller::HTTP_OK);
@@ -61,9 +63,9 @@ class Search extends REST_Controller
      */
     public function index_get()
     {
-        $data['srch_inp'] = $this->input->get("srch_pr_inp");
+        $srch_pr_inp = $this->replaceStr($this->input->get("srch_pr_inp"));
         $res = $this->product->search_for_prod(
-            $this->input->get("srch_pr_inp"),
+            $srch_pr_inp,
             $min_price = '',
             $max_price = '',
             $this->input->get("user_id")
@@ -93,5 +95,25 @@ class Search extends REST_Controller
         ];
 
         $this->response($result, REST_Controller::HTTP_OK);
+    }
+
+    private function replaceStr($searchText)
+    {
+        $cyr=array(
+            "Щ", "Ш", "Ч","Ц", "Ю", "Я", "Ж","А","Б","В","Г","Д","Е","Ё","З","И","Й","К","Л","М","Н", "О","П","Р",
+            "С","Т","У","Ф","Х","Ь","Ы","Ъ", "Э","Є", "Ї","І", "щ", "ш", "ч","ц", "ю", "я", "ж","а","б","в", "г","д",
+            "е","ё","з","и","й","к","л","м","н", "о","п","р","с","т","у","ф","х","ь","ы","ъ", "э","є", "ї","і"
+        );
+
+        $lat=array(
+            "Shch","Sh","Ch","C","Yu","Ya","J","A","B","V","G","D","E","E","Z","I","y","K","L","M","N", "O","P","R","S",
+            "T","U","F","H","", "Y","","E","E","Yi","I","shch","sh","ch","c","Yu","Ya","j","a","b","v", "g","d","e","e",
+            "z","i","y","k","l","m","n", "o","p","r","s","t","u","f","h", "", "y","" ,"e","e","yi","i"
+        );
+
+        $searchText = str_replace($lat,$cyr,$searchText);
+        $searchText = str_replace("_"," ",$searchText);
+
+        return $searchText;
     }
 }
